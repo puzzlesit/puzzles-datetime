3fc912ddb1446eccc72d50c723432364600f5202
tihynihy <tihynihy@gmail.com>
2023-07-06 22:30:53 +0200

    Demo just to see the progress

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <puzzles-datetime v-model="date" @display="displayDate = arguments[0]">
+    <puzzles-datetime :custom-style="customStyle" v-model="date" @display="displayDate = arguments[0]">
         <input type="text"
                v-model="displayDate"
                placeholder="Select a date"
@@ -46,6 +46,17 @@ export default { // This page is only for testing
   name: 'App',
   data() {
     return {
+      customStyle: {
+        backgroundColor: 'white',
+        textColor: 'black',
+        calendarHeaderBackgroundColor: '#e5e5e5',
+        calendarDatesBackgroundColor: 'white',
+        calendarSelectedBackgroundColor: '#736cf0',
+        backgroundLayerBackgroundColor: 'black',
+        backgroundLayerOpacity: 0,
+        pickerMaxWidth: '320px',
+        fontSize: '1rem'
+      },
       date: '2022-10-09',
       displayDate: '',
       time: '',
