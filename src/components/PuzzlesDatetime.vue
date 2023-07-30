b18239a2051f8c072fddf8f3fa976a50d25caeba
tihynihy <tihynihy@gmail.com>
2023-07-30 18:57:03 +0200

    latest patch on the select day

--- src/components/PuzzlesDatetime.vue
+++ src/components/PuzzlesDatetime.vue
@@ -677,8 +677,8 @@ export default {
     }
 
     td.selected {
-      display: inline-block;
-      min-width: 2rem;
+      display: table-caption;
+      min-width: 2.2rem;
       padding: 0 0.5rem;
       box-sizing: border-box;
     }
